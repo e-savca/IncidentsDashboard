986f05e446a95b8a766c035bf1b02a304c550bd9
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-03-04 11:58:39 +0200

    Added Application Layer

---
+++ src/Application/Interfaces/IDatabaseService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Users;
+using System.Data.Entity;
+namespace Application.Interfaces
+{
+    public interface IDatabaseService
+    {
+        IDbSet<User> Users { get; set; }
+
+        void Save();
+    }
+}
---
+++ src/Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.Commands.CreateUser
+{
+    public class CreateUserCommand : ICreateUserCommand
+    {
+        private readonly IDatabaseService _database;
+        public CreateUserCommand(
+            IDatabaseService database)
+        {
+            _database = database;
+            
+        }
+        public void Execute(CreateUserModel model) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
