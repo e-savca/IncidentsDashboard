3064fcd587c4358e555f75b466dafc497cc59761
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-03-04 11:58:52 +0200

    Added Persistance Layer

---
+++ src/Persistance/DatabaseService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public class DatabaseService : DbContext, IDatabaseService
+    {
+        public IDbSet<User> Users { get; set; }
+
+        public DatabaseService() : base("name=DefaultConnection")
+        {
+            
+        }
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
