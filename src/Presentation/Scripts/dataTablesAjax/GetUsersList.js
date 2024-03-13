52840ded050e506ccc1f22a663afa201b23a5fb0
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-03-13 15:18:57 +0200

    minor changes

--- src/Presentation/Scripts/dataTablesAjax/GetUsersList.js
+++ src/Presentation/Scripts/dataTablesAjax/GetUsersList.js
@@ -2,22 +2,22 @@
     $('#usersTable').DataTable(
         {
             /*serverSide: true,*/
-            "ajax": {
-                "url": "/Admin/GetUsersListAsync",
-                "type": "GET",
-                "datatype": "json",
-                "dataSrc": ""
+            'ajax': {
+                'url': '/Admin/GetUsersListAsync',
+                'type': 'GET',
+                'datatype': 'json',
+                'dataSrc': ''
             },
-            "columns": [
-                { "data": "Username" },
-                { "data": "FirstName" },
-                { "data": "LastName" },
-                { "data": "Email" },
-                { "data": "IsActive" },
+            'columns': [
+                { 'data': 'Username' },
+                { 'data': 'FirstName' },
+                { 'data': 'LastName' },
+                { 'data': 'Email' },
+                { 'data': 'IsActive' },
                 {
-                    "data": "UserRoles",
-                    "render": function (data, type, row) {
-                        return data.join(", ");
+                    'data': 'UserRoles',
+                    'render': function (data, type, row) {
+                        return data.join(', ');
 
                     }
                 }
