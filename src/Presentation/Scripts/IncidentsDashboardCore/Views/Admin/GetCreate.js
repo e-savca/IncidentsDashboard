5243e3dbcde490b36ddefffdaed1d2b5bf1d3dc2
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-03-15 15:31:23 +0200

    Moved the js code from the view in a separated file. Added the feature which will clear alert messages for properties in the form in Admin/GetCreate View

---
+++ src/Presentation/Scripts/IncidentsDashboardCore/Views/Admin/GetCreate.js
@@ -0,0 +1,61 @@
+﻿// model.IsActive property
+document.querySelectorAll('input[name="btnradio"]').forEach(function (radio) {
+    radio.addEventListener('change', function () {
+        document.getElementById('isActiveField').value = this.value;
+    });
+});
+
+
+// send data to server and handle response
+document.getElementById('createUserForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var formData = new FormData(this);
+
+    fetch(this.action, {
+        method: this.method,
+        body: formData,
+        headers: {
+            'RequestVerificationToken': document.querySelector('input[name="__RequestVerificationToken"]').value
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                window.location.href = '#Admin';
+            }
+            else {
+                // Display error messages
+                // foreach in data.message call appendAlert
+                clearAlertValidationMessage();
+                data.errors.forEach((item) => {
+
+                    appendAlertValidationMessage(item.message, item.propertyName)
+                });
+
+                //appendAlertValidationMessage(data.message, data.messageType, data.element)
+            }
+        });
+});
+
+// Append alert message to the page
+const appendAlertValidationMessage = (message, element) => {
+    // Find the validation span for the given element
+    let validationSpan = document.querySelector(`span[data-valmsg-for="${element}"]`);
+
+    // Set the error message and class
+    validationSpan.textContent = message;
+    validationSpan.className = `field-validation-error text-danger`;
+}
+
+// Clear append alert message
+const clearAlertValidationMessage = () => {
+    // Find the validation span for all the elements
+    let validationSpans = document.querySelectorAll(`span[data-valmsg-for]`);
+
+    // Clear the error message and class
+    validationSpans.forEach((item) => {
+        item.textContent = '';
+        item.className = '';
+    });
+}
