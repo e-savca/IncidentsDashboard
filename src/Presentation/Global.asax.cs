5ac67126055850b9322c6cbe8d7d684cbf8b4938
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-03-20 15:06:59 +0200

    Install MediatR and MediatR.Ninject

--- src/Presentation/Global.asax.cs
+++ src/Presentation/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using FluentValidation.Mvc;
+using MediatR.Ninject;
 
 namespace Presentation
 {
@@ -18,12 +19,17 @@ namespace Presentation
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             
+            // FluentValidation setup
             FluentValidationModelValidatorProvider.Configure();
 
 
             // add dependencies
             NinjectModule registrations = new NinjectRegistrations();
             var kernel = new StandardKernel(registrations);
+
+            // Add MediatR
+            kernel.BindMediatR();
+
             DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }
     }
