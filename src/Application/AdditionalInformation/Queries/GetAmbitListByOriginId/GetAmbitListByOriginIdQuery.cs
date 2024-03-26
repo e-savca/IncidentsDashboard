ed7e33d1bc66d9cb9d0602829b1e4b9d479bbee9
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-03-26 13:50:17 +0200

    Added GetAmbitListByOriginId Query, Handler, Model and View

---
+++ src/Application/AdditionalInformation/Queries/GetAmbitListByOriginId/GetAmbitListByOriginIdQuery.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using MediatR;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.AdditionalInformation.Queries.GetAmbitListByOriginId
+{
+    public class GetAmbitListByOriginIdQuery : IRequest<List<AmbitListByOriginIdItemModel>>
+    {
+        public int OriginId { get; set; }
+    }
+    public class GetAmbitListByOriginIdHandler : IRequestHandler<GetAmbitListByOriginIdQuery, List<AmbitListByOriginIdItemModel>>
+    {
+        private readonly IDatabaseService _database;
+        public GetAmbitListByOriginIdHandler(
+            IDatabaseService database
+            )
+        {
+            _database = database;
+        }
+        public async Task<List<AmbitListByOriginIdItemModel>> Handle(GetAmbitListByOriginIdQuery request, CancellationToken cancellationToken)
+        {
+            // get data from database
+            var ambits = await _database.Ambits.
+                Include(i => i.originToAmbits)
+                .ToListAsync(cancellationToken);
+
+
+            // map data
+            var ambitDtos = ambits.Where(a => a.originToAmbits.Any(aa => aa.OriginId == request.OriginId))
+                .Select(a => new AmbitListByOriginIdItemModel
+            {
+                Id = a.Id,
+                Name = a.Name
+            }).ToList();
+            return ambitDtos;
+        }
+    }
+}
