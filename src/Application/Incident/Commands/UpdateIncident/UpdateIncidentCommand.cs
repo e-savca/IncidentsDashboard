0dff3c4a4a0e6a6784a34df508e558903a59c926
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-03-27 15:22:08 +0200

    Added UpdateIncident Command, Model and ModelValidator in Application Layer

---
+++ src/Application/Incident/Commands/UpdateIncident/UpdateIncidentCommand.cs
@@ -0,0 +1,63 @@
+﻿using Application.Interfaces;
+using MediatR;
+using System;
+using System.Data.Entity.Migrations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Incident.Commands.UpdateIncident
+{
+    public class UpdateIncidentCommand : IRequest<UpdateIncidentModel>
+    {
+        public UpdateIncidentModel IncidentModel { get; set; }
+    }
+
+    public class UpdateIncidentHandler : IRequestHandler<UpdateIncidentCommand, UpdateIncidentModel>
+    {
+        private readonly IDatabaseService _database;
+        public UpdateIncidentHandler(
+            IDatabaseService database
+            )
+        {
+            _database = database;
+        }
+        public async Task<UpdateIncidentModel> Handle(UpdateIncidentCommand request, CancellationToken cancellationToken)
+        {
+            var model = request.IncidentModel;
+            // get current incident from database
+            var incident = await _database.Incidents
+                           .FindAsync(cancellationToken, model.Id);
+            if (incident == null)
+            {
+                return null;
+            }
+            // update incident
+            incident.CallCode = model.CallCode;
+            incident.SubsystemCode = model.SubsystemCode;
+            // parse data from string
+            incident.OpenedDate = DateTime.Parse(model.OpenedDate);
+            incident.ClosedDate = DateTime.Parse(model.ClosedDate);
+            incident.RequestType = model.RequestType;
+            incident.ApplicationType = model.ApplicationType;
+            incident.Urgency = model.Urgency;
+            incident.SubCause = model.SubCause;
+            incident.Summary = model.Summary;
+            incident.Description = model.Description;
+            incident.Solution = model.Solution;
+            incident.OriginId = model.OriginId;
+            incident.AmbitId = model.AmbitId;
+            incident.ThreatId = model.ThreatId;
+            incident.IncidentTypeId = model.IncidentTypeId;
+            incident.ScenarioId = model.ScenarioId;
+            incident.ThreatId = model.ThreatId;
+
+            // update incident in database
+            _database.Incidents.AddOrUpdate(incident);
+
+            // save changes
+            await _database.SaveAsync(cancellationToken);
+
+            return model;
+        }
+    }
+}
---
+++ src/Application/Incident/Commands/UpdateIncident/UpdateIncidentModelValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Application.Incident.Commands.UpdateIncident
+{
+    public class UpdateIncidentModelValidator : AbstractValidator<UpdateIncidentModel>
+    {
+        public UpdateIncidentModelValidator()
+        {
+            RuleFor(x => x.CallCode).NotEmpty().WithMessage("Call code is required");
+            RuleFor(x => x.SubsystemCode).NotEmpty().WithMessage("Subsystem code is required");
+            RuleFor(x => x.OpenedDate).NotEmpty().WithMessage("Opened date is required");
+            RuleFor(x => x.ClosedDate).NotEmpty().WithMessage("Closed date is required");
+            RuleFor(x => x.RequestType).NotEmpty().WithMessage("Request type is required");
+            RuleFor(x => x.ApplicationType).NotEmpty().WithMessage("Application type is required");
+            RuleFor(x => x.Urgency).NotEmpty().WithMessage("Urgency is required");
+            RuleFor(x => x.SubCause).NotEmpty().WithMessage("Sub cause is required");
+            RuleFor(x => x.Summary).NotEmpty().WithMessage("Summary is required");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+            RuleFor(x => x.Solution).NotEmpty().WithMessage("Solution is required");
+            RuleFor(x => x.OriginId).NotEmpty().WithMessage("Origin is required");
+            RuleFor(x => x.AmbitId).NotEmpty().WithMessage("Ambit is required");
+            RuleFor(x => x.IncidentTypeId).NotEmpty().WithMessage("Incident type is required");
+            RuleFor(x => x.ScenarioId).NotEmpty().WithMessage("Scenario is required");
+            RuleFor(x => x.ThreatId).NotEmpty().WithMessage("Threat is required");
+        }
+    }
+}
