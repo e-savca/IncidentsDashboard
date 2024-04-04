79cd333f514c91c1be726e3f9bfe04d702da17cd
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2024-04-04 15:24:08 +0300

    Added DataTablesResponseModel

---
+++ src/Common/Models/DataTableResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Models
+{
+    public class DataTableResponseModel<T> where T : class
+    {
+        public string draw {  get; set; }
+        public int recordsTotal { get; set; }
+        public int recordsFiltered { get; set; }
+        public List<T> data {  get; set; } 
+    }
+}
